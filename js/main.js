ae605ceb5a9838b2f25b782ef395911289234eac
Driss Chelouati <driss.chelouati@archive-dynamics.com>
2018-04-10 00:57:26 +0100

    wip on landing page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    
+
     "use strict";
-    
+
     //Page loader
     if ($('.pageloader').length) {
 
@@ -41,7 +41,7 @@ $(document).ready(function() {
             }
         });
     }
-    
+
     //Pop Dropdowns
     $('.dropdown-trigger').on('click', function(event) {
         event.stopPropagation();
@@ -57,4 +57,128 @@ $(document).ready(function() {
         //} 
     });
     
+    //Navigation Tabs
+    $('.flying-tabs li').on('click', function() {
+        var tab_id = $(this).attr('data-tab');
+
+        $(this).siblings('li').removeClass('is-active');
+        $(this).find('#flying-tabs-content').children('.navtab-content').removeClass('is-active');
+
+        $(this).addClass('is-active');
+        $("#"+tab_id).addClass('is-active');
+    })
+
+    /* ---- particles.js config ---- */
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 50,
+                "density": {
+                    "enable": true,
+                    "value_area":1000
+                }
+            },
+            "color": {
+                "value": ["#5507fc"]
+            },
+
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 5,
+                    "color": "#5507fc"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.6,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 2,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 120,
+                "color": "#5507fc",
+                "opacity": 0.2,
+                "width": 1.6
+            },
+            "move": {
+                "enable": true,
+                "speed": 3,
+                "direction": "top",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": false
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
 })
