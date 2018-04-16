e61db6b2a8d5383aaca6ba19fcc233cd42e9a90f
Driss Chelouati <driss.chelouati@archive-dynamics.com>
2018-04-16 21:28:57 +0100

    almost finished roadmap page

--- js/main.js
+++ js/main.js
@@ -97,10 +97,10 @@ $(document).ready(function() {
             $('.tab-content-wrapper').removeClass('is-flex-mobile');
         }
     })
-    
+
     //Aos
     AOS.init();
-    
+
     //Documentation languages toggle
     if ($('.token-documentation').length) {
         $('.token-documentation ul li').on('click', function(){
@@ -108,7 +108,45 @@ $(document).ready(function() {
             $(this).addClass('is-active');
         })
     }
-    
+
+    //Anchor scroll
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .on('click', function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 550, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+
 
     /* ---- particles.js config ---- */
 
