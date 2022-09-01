b0b93aad9835c91f3926f45140982ef46d1e8d05
Driss Chelouati <driss.chelouati@gmail.com>
2022-09-01 15:55:48 +0100

    feat: update dependencies

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -7265,7 +7265,7 @@
           return {
             setupCountdown: function setupCountdown() {
               // Set the date we're counting down to
-              var countDownDate = new Date("Oct 24, 2021 07:00:00").getTime(); // Update the count down every 1 second
+              var countDownDate = new Date("Oct 24, 2023 07:00:00").getTime(); // Update the count down every 1 second
 
               var x = setInterval(function () {
                 // Get todays date and time
