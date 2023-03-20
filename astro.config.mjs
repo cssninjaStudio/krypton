c6647a9e21c5aaa8682d1e47cb803ac52811a139
Sacha STAFYNIAK <sacha@digisquad.io>
2023-03-20 14:07:01 +0100

    feat: upgrade dependencies + add unplugin-fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import Unfonts from 'unplugin-fonts/astro'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    Unfonts({
+      google: {
+        families: [
+          'Cabin',
+          {
+            name: 'Open Sans',
+            styles: 'wght@300;400',
+          },
+        ],
+      },
+    })
+  ]
+});
