a8c9e314cbab34002d3c218235e3293f9c0d0b57
Sacha STAFYNIAK <sacha@digisquad.io>
2023-09-17 18:07:43 +0200

    feat: use variable fonts from fontsources

--- src/js/libs/components/tabs/tabs.js
+++ src/js/libs/components/tabs/tabs.js
@@ -8,7 +8,7 @@ export function initTabs() {
 
         setupCountdown() {
             // Set the date we're counting down to
-            var countDownDate = new Date("Oct 24, 2021 07:00:00").getTime();
+            var countDownDate = new Date().getTime() + (1000 * 60 * 60 * 24 * 7); // 7 days from now
       
             // Update the count down every 1 second
             var x = setInterval(function () {
